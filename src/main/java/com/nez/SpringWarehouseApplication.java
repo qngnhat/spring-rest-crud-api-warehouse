c19bc838e5761d416cb65fa2721e9130da3f57e9
qngnhat <qngnhat@gmail.com>
2021-04-17 23:28:12 +0700

    push project

---
+++ src/main/java/com/nez/SpringWarehouseApplication.java
@@ -0,0 +1,13 @@
+package com.nez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWarehouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWarehouseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nez/SpringWarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWarehouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
