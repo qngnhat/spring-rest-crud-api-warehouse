010e40c6e336ec556ecee6adaf30386e3c3b59be
qngnhat <qngnhat@gmail.com>
2021-04-20 05:48:10 +0700

    them data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,58 @@
+insert into customers values (1, 'James', 'Carter','12 To Ky','Ho Chi Minh','0123123123');
+insert into customers values (2, 'Helen', 'Leary','25 Dong Bac','Ho Chi Minh','0123123123');
+insert into customers values (3, 'Linda', 'Douglas','122 Nguyen Kiem','Ho Chi Minh','0123123123');
+insert into customers values (4, 'Rafael', 'Ortega','23 Dong Da', 'Ha Noi','0123123123');
+insert into customers values (5, 'Henry', 'Stevens','123 Pham Van Dong','Ho Chi Minh','0123123123');
+insert into customers values (6, 'Sharon', 'Jenkins','87 Quang Trung','Ha Noi','0123123123');
+
+insert into employees values (1, 'George', 'Franklin', '110 W. Liberty St.','1992-06-12', 'Franklin@gmail.com', 'Male', 1, '123456', '123123123', 'Franklin.jpg', 'ROLE_Admin', '0123123123', 'Franklin');
+insert into employees values (2, 'Betty', 'Davis', '638 Cardinal Ave.','1992-06-12', 'Davis@gmail.com', 'Male', 1, '123456', '123123123', 'Davis.jpg', 'ROLE_User', '0123123123', 'Davis');
+insert into employees values (3, 'Eduardo', 'Rodriquez', '2693 Commerce St.', '1992-06-12', 'Rodriquez@gmail.com', 'Female', 1, '123456', '123123123', 'Rodriquez.jpg', 'ROLE_User', '0123123123', 'Rodriquez');
+insert into employees values (4, 'Harold', 'Davie', '563 Friendly St.','1992-06-12', 'Davie@gmail.com', 'Male', 1, '123456', '123123123', 'Davie.jpg', 'ROLE_User', '0123123123', 'Davie');
+insert into employees values (5, 'Peter', 'McTavish', '2387 S. Fair Way', '1992-06-12', 'McTavish@gmail.com', 'Male', 1, '123456', '123123123', 'McTavish.jpg', 'ROLE_User', '0123123123', 'McTavish');
+insert into employees values (6, 'Jean', 'Coleman', '105 N. Lake St.','1992-06-12', 'Coleman@gmail.com', 'Male', 1, '123456', '123123123', 'Coleman.jpg', 'ROLE_User', '0123123123', 'Coleman');
+insert into employees values (7, 'Jeff', 'Black', '1450 Oak Blvd.','1992-06-12', 'Black@gmail.com', 'Male', 1, '123456', '123123123', 'Black.jpg', 'ROLE_User', '0123123123', 'Black');
+
+insert into suppliers values (1, 'Koba', 'Cessar', '15 Ton Duc Thang', 'Ha Noi', '0123123123');
+insert into suppliers values (2, 'Noah', 'Emma','122 Nguyen Kiem','Ho Chi Minh', '0123123123');
+insert into suppliers values (3, 'Oliver', 'Ava','87 Quang Trung','Ha Noi', '0123123123');
+insert into suppliers values (4, 'William', 'Sophia', '155 Pham Van Dong', 'Ha Noi', '0123123123');
+insert into suppliers values (5, 'Elijah', 'Benjamin', '98 Ly Chinh Thang', 'Ha Noi', '0123123123');
+insert into suppliers values (6, 'James', 'Lucas', '33 Nguyen Kiem', 'Ho Chi Minh', '0123123123');
+
+insert into categories values (1, 'Original');
+insert into categories values (2, 'Football');
+insert into categories values (3, 'Running');
+insert into categories values (4, 'Tranning');
+insert into categories values (5, 'Outdoor');
+insert into categories values (6, 'Tennis');
+
+insert into products values (1, 'zx 8000 superearth shoes', 'zx-8000-superearth-shoes.jpg', 3400000, 399, 1, 1);
+insert into products values (2, 'samba vegan shoes', 'samba-vegan-shoes.jpg', 2500000, 399, 1, 1);
+insert into products values (3, 'copa sense.3 turf boots', 'copa-sense.3-turf-boots.jpg', 1800000, 399, 2, 2);
+insert into products values (4, 'nemeziz.3 firm ground boots', 'nemeziz.3-firm-ground-boots.jpg', 2000000, 399, 2, 2);
+insert into products values (5, 'predator mutator 20.3 low firm ground boots', 'predator-mutator-20.3-low-firm-ground-boots.jpg', 1800000, 399, 2, 2);
+insert into products values (6, 'copa 20.3 firm ground boots', 'copa-20.3-firm-ground-boots.jpg', 1500000, 399, 2, 2);
+insert into products values (7, 'predator mutator 20.1 low firm ground boots', 'predator-mutator-20.1-low-firm-ground-boots.png', 2200000, 399, 2, 2);
+insert into products values (8, 'ultraboost dna shoes', 'ultraboost-dna-shoes.jpg', 4200000, 399, 3, 3);
+insert into products values (9, 'ultraboost 21 x parley shoes', 'ultraboost-21-x-parley-shoes.jpg', 5000000, 399, 3, 4);
+insert into products values (10, 'response super shoes', 'response-super-shoes.jpg', 2100000, 399, 3, 4);
+insert into products values (11, 'ultraboost 21 shoes', 'ultraboost-21-shoes.jpg', 5000000, 399, 3, 5);
+insert into products values (12, 'superstar slip on shoes', 'superstar-slip-on-shoes.jpg', 2400000, 399, 4, 5);
+insert into products values (13, 'stan smith shoes', 'stan-smith-shoes.jpg', 2400000, 399, 4, 6);
+insert into products values (14, 'solematch bounce tennis shoes', 'solematch-bounce-tennis-shoes.jpg', 3000000, 399, 5, 4);
+insert into products values (15, 'courtjam bounce shoes', 'courtjam-bounce-shoes.png', 2400000, 399, 6, 4);
+
+insert into orders values (1, '2021-04-12 15:00:00', 4300000, '2021-04-14', 1, 1);
+insert into orders values (2, '2021-04-12 09:20:00', 4300000, '2021-04-14', 1, 2);
+insert into orders values (3, '2021-04-12 10:30:00', 4300000, '2021-04-14', 2, 3);
+insert into orders values (4, '2021-04-12 11:40:00', 4300000, '2021-04-14', 3, 4);
+
+insert into order_items values (1, 1, 1, 2);
+insert into order_items values (2, 1, 1, 3);
+insert into order_items values (3, 1, 2, 2);
+insert into order_items values (4, 1, 2, 3);
+insert into order_items values (5, 1, 3, 2);
+insert into order_items values (6, 1, 3, 3);
+insert into order_items values (7, 1, 4, 2);
+insert into order_items values (8, 1, 4, 3);
