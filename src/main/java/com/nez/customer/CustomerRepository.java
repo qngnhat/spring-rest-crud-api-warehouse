dac33857fd3d58f535095bffe39bf1adfcafcd06
qngnhat <qngnhat@gmail.com>
2021-04-20 05:51:47 +0700

    edit Customer repo

--- src/main/java/com/nez/customer/CustomerRepository.java
+++ src/main/java/com/nez/customer/CustomerRepository.java
@@ -1,23 +1,13 @@
 package com.nez.customer;
 
-import java.util.Collection;
-
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.Repository;
-import org.springframework.data.repository.query.Param;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-public interface CustomerRepository extends Repository<Customer, Integer>{
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Integer>{
 	
-	@Query("SELECT DISTINCT customer FROM Customer customer LEFT JOIN FETCH customer.orders WHERE customer.lastName LIKE :lastName%")
 	@Transactional(readOnly = true)
-	Collection<Customer> findByLastName(@Param("lastName") String lastName);
+	Customer findCustomerById(Integer id);
 	
-	
-	@Query("SELECT customer FROM Customer customer left join fetch customer.orders WHERE customer.id =:id")
-	@Transactional(readOnly = true)
-	Customer findById(@Param("id") Integer id);
-	
-	
-	void save(Customer customer);
 }
