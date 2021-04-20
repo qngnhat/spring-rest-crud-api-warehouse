253081eaf2b546b8ebae4c771342cbc4e49a5536
qngnhat <qngnhat@gmail.com>
2021-04-20 09:29:19 +0700

    remove trash category

--- src/main/java/com/nez/product/Category.java
+++ src/main/java/com/nez/product/Category.java
@@ -1,10 +1,6 @@
 package com.nez.product;
 
-import java.util.Set;
-
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import com.nez.model.NamedEntity;
@@ -12,6 +8,4 @@ import com.nez.model.NamedEntity;
 @Entity
 @Table(name = "categories")
 public class Category extends NamedEntity{
-	@OneToMany(cascade = CascadeType.ALL, mappedBy = "category")
-	private Set<Product> products;
 }
