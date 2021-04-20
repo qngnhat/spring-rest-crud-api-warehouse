06a645db0ccb2519ad2d349570e217d913f27fbe
qngnhat <qngnhat@gmail.com>
2021-04-20 09:10:25 +0700

    chinh sua CategoryRepository

--- src/main/java/com/nez/product/CategoryRepository.java
+++ src/main/java/com/nez/product/CategoryRepository.java
@@ -1,5 +1,9 @@
 package com.nez.product;
 
-public interface CategoryRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Integer>{
 
 }
