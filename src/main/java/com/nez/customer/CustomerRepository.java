3714bbbda8ae643567bb23ac767e29242d1072c7
qngnhat <qngnhat@gmail.com>
2021-04-20 08:37:41 +0700

    edit customer repo

--- src/main/java/com/nez/customer/CustomerRepository.java
+++ src/main/java/com/nez/customer/CustomerRepository.java
@@ -2,12 +2,9 @@ package com.nez.customer;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Integer>{
 	
-	@Transactional(readOnly = true)
-	Customer findCustomerById(Integer id);
 	
 }
