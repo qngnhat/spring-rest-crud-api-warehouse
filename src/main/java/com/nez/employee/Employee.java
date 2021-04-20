1e45ec3e410a2e4af7b14f82032fac28245c46f4
qngnhat <qngnhat@gmail.com>
2021-04-20 08:40:39 +0700

    edit employee entity

--- src/main/java/com/nez/employee/Employee.java
+++ src/main/java/com/nez/employee/Employee.java
@@ -1,20 +1,13 @@
 package com.nez.employee;
 
-import java.util.Set;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import com.nez.model.BaseAccount;
-import com.nez.order.Order;
 
 @Entity
 @Table(name = "employees")
 public class Employee extends BaseAccount{
-	
-	@OneToMany(cascade = CascadeType.ALL, mappedBy = "employee")
-	private Set<Order> orders;
 	
 }
