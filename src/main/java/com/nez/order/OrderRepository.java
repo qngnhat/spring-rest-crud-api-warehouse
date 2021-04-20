53038301ff8399705f35e503f5415c3625bb6f51
qngnhat <qngnhat@gmail.com>
2021-04-20 08:53:27 +0700

    edit OrderRepository

--- src/main/java/com/nez/order/OrderRepository.java
+++ src/main/java/com/nez/order/OrderRepository.java
@@ -1,7 +1,9 @@
 package com.nez.order;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OrderRepository extends JpaRepository<Order, Integer>{
 
 }
