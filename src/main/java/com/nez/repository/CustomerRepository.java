4b07cac7f14b397f17fa67425cdc7aae1c21b82c
qngnhat <qngnhat@gmail.com>
2021-04-22 06:03:39 +0700

    change source

--- src/main/java/com/nez/repository/CustomerRepository.java
+++ src/main/java/com/nez/repository/CustomerRepository.java
@@ -1,6 +1,8 @@
 package com.nez.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.nez.entities.Customer;
@@ -8,4 +10,7 @@ import com.nez.entities.Customer;
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Integer>{
 	public Customer findById(int id);
+	
+	@Query("select c from Customer c where c.id=:id")
+	public Customer getCustomerById(@Param("id") int id);
 }
